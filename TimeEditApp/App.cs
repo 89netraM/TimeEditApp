3ce383936d70b0a6337c47a469eae617e8214dfa
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-31 11:13:51 +0100

    Added a Menu with and Exit action

--- TimeEditApp/App.cs
+++ TimeEditApp/App.cs
@@ -1,4 +1,5 @@
-﻿using Terminal.Gui;
+﻿using MoreTec.TimeEditApp.Views;
+using Terminal.Gui;
 
 namespace MoreTec.TimeEditApp
 {
@@ -6,12 +7,27 @@ namespace MoreTec.TimeEditApp
 	{
 		private const string WindowTitle = "TimeEditApp";
 
+		private readonly Menu menu = new Menu();
+
 		public App() : base(WindowTitle)
 		{
 			X = 0;
-			Y = 1;
+			Y = 0;
 			Width = Dim.Fill();
 			Height = Dim.Fill();
+
+			InitMenu();
+		}
+
+		private void InitMenu()
+		{
+			this.Add(menu);
+			menu.Exit += Menu_Exit;
+		}
+
+		private void Menu_Exit()
+		{
+			Running = false;
 		}
 	}
 }
---
+++ TimeEditApp/Views/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Terminal.Gui;
+
+namespace MoreTec.TimeEditApp.Views
+{
+	class Menu : MenuBar
+	{
+		public event Action Exit;
+		public event Action Search;
+		public event Action Schedule;
+
+		public Menu() : base(null)
+		{
+			Menus = new MenuBarItem[]
+			{
+				new MenuBarItem("_File", new MenuItem[]
+				{
+					new MenuItem("_Exit", null, () => Exit?.Invoke())
+				}),
+				new MenuBarItem("_View", new MenuItem[]
+				{
+					new MenuItem("_Search", "Search for schedules", () => Search?.Invoke()),
+					new MenuItem("_Schedule", "Display a schedule", () => Schedule?.Invoke()),
+				})
+			};
+		}
+	}
+}
