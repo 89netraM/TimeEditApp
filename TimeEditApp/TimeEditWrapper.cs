cfd300562bf085357a0a597ce1325d42874069e0
Mårten Åsberg <marten.asberg@outlook.com>
2019-10-31 13:43:06 +0100

    Created a wrapper for the TimeEdit API

---
+++ TimeEditApp/TimeEditWrapper.cs
@@ -0,0 +1,26 @@
+﻿using MoreTec.TimeEditApi;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace MoreTec.TimeEditApp
+{
+	static class TimeEditWrapper
+	{
+		private static readonly TimeEdit instance = new TimeEdit("https://cloud.timeedit.net/chalmers/web/public/");
+
+		public static Task<IImmutableList<ScheduleType>> GetScheduleTypes()
+		{
+			return instance.GetScheduleTypes();
+		}
+
+		public static Task<IImmutableList<SearchItem>> Search(string query, params int[] types)
+		{
+			return instance.Search(query, types);
+		}
+
+		public static Task<Schedule> GetSchedule(int scheduleId)
+		{
+			return instance.GetSchedule(scheduleId);
+		}
+	}
+}
